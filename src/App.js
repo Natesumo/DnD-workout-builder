ff3d4f92992f61c46121b7f0e7e4e3ef513feeab
Natesumo <natesumo@yahoo.com>
2020-03-11 23:47:36 -0400

    Initial Commit
    create-react-app setup.
    some AddToList functionality.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import WorkoutList from './components/WorkoutList'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>APP</h1>
+      <WorkoutList />
+    
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WorkoutList.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export default class WorkoutList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userInput: '',
+            list: []
+        }
+    }
+
+    changeUserInput(input) {
+        this.setState({
+            userInput: input
+        }, ()=> console.log(input))
+    }
+
+    addToList(input){
+        let listArray = this.state.list;
+        listArray.push(input);
+        this.setState({
+            list: listArray,
+            userInput: ''
+        })
+    }
+
+    render() {
+        return (
+            <div className="workout-list">
+                <input 
+                    onChange={ (e)=>this.changeUserInput(e.target.value) }
+                    value={this.state.userInput} 
+                    type="text" 
+                />
+                <button onClick={ ()=> this.addToList(this.state.userInput)}>
+                        Add Workout
+                </button>
+                <ul>
+                    {this.state.list.map( (val)=> <li key={val}>{val}</li> )}
+                </ul>
+            </div>
+        )
+    }
+};
