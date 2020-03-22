b3f754b7cfb928beb7bd9ed489f3c35eda0843a1
Natesumo <natesumo@yahoo.com>
2020-03-21 21:52:21 -0400

    Updates

---
+++ precache-manifest.d2ccd54ef071d6bb8a63e2ecdb70494b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0587eceb7d9e6ea56c782440bab285",
+    "url": "/DnD-workout-builder/index.html"
+  },
+  {
+    "revision": "ec57c1150424b1e64431",
+    "url": "/DnD-workout-builder/static/css/main.a3638d16.chunk.css"
+  },
+  {
+    "revision": "0d4e9db7529e117d5c3c",
+    "url": "/DnD-workout-builder/static/js/2.1a6d3038.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/DnD-workout-builder/static/js/2.1a6d3038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec57c1150424b1e64431",
+    "url": "/DnD-workout-builder/static/js/main.a9a7e0dd.chunk.js"
+  },
+  {
+    "revision": "8ab6a3b3469d6b24e84a",
+    "url": "/DnD-workout-builder/static/js/runtime-main.061d762a.js"
+  }
+]);
